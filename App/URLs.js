53ab4d448a9b0bc16cfc722a028bea035a4cc0d2
Chance Hudson <jchancehud@gmail.com>
2019-03-17 03:28:18 -0500

    Add count url

--- App/URLs.js
+++ App/URLs.js
@@ -1,11 +1,13 @@
 const BASE_URL = 'https://shielded-sea-32169.herokuapp.com/api';
-const NEW_BASE_URL = 'https://api.gocert.io';
+const NEW_BASE_URL = 'http://localhost:4000';
+// const NEW_BASE_URL = 'https://api.gocert.io';
 
 export default {
   users: {
     _: `${NEW_BASE_URL}/users`,
     login: `${NEW_BASE_URL}/users/login`,
     events: `${NEW_BASE_URL}/users/events`,
+    eventsCount: `${NEW_BASE_URL}/users/events/count`,
   },
   events: {
     _: `${NEW_BASE_URL}/events`,
