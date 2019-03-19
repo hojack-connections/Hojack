b2d7efa554ac8886da93706fe60a8f4a0667451c
Chance Hudson <jchancehud@gmail.com>
2019-03-18 23:08:02 -0500

    Remove unused redux sagas, reducers, action

--- App/sagas/config.js
+++
@@ -1,9 +0,0 @@
-const BASE_PATH = 'https://shielded-sea-32169.herokuapp.com/api/';
-// const BASE_PATH = 'https://localhost:7001/';
-
-export default (API_PATH = {
-  auth: BASE_PATH + 'users',
-  login: BASE_PATH + 'users/login',
-  attendee: BASE_PATH + 'attendees',
-  event: BASE_PATH + 'events',
-});
